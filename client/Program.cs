5b43add3f3bf1d4a79f55f87b608714ab0502ff8
GTT\wdgutierrez <wdgutierrez@gtt.es>
2021-12-02 15:47:23 -0600

    Unary API Client implementation

--- client/Program.cs
+++ client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Dummy;
+using Greet;
 using Grpc.Core;
 using System;
 using System.Collections.Generic;
@@ -21,8 +22,20 @@ namespace client
                     Console.WriteLine("The client connected successfully");
             });
 
-            var client = new DummyService.DummyServiceClient(channel);
+            // Testing GreetingService
+            //var client = new DummyService.DummyServiceClient(channel);
+            var client = new GreetingService.GreetingServiceClient(channel);
 
+            var greeting = new Greeting()
+            {
+                FirstName = "Deyanira",
+                LastName = "Gutierrez"
+            };
+
+            var request = new GreetingRequest() { Greeting = greeting };
+            var response = client.Greet(request);
+
+            Console.WriteLine("Response: " + response.Result);
             channel.ShutdownAsync().Wait();
             Console.ReadKey();  
         }
