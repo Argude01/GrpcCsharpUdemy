097d49118ed08de3a25f00c76470381e86a0eac7
Deyanira Gutiérrez Arteaga <deyaniragutierrezarteaga@gmail.com>
2021-12-11 10:39:44 -0600

    gRPC Reflection and Evans CLI

--- server/Program.cs
+++ server/Program.cs
@@ -2,6 +2,8 @@
 using Greet;
 using Greetdeadline;
 using Grpc.Core;
+using Grpc.Reflection;
+using Grpc.Reflection.V1Alpha;
 using Sqrt;
 using System;
 using System.Collections.Generic;
@@ -21,23 +23,28 @@ namespace server
             Server server = null;
             try
             {
-                var serverCert = File.ReadAllText("ssl/server.crt");
-                var serverKey = File.ReadAllText("ssl/server.key");
-                var keypair = new KeyCertificatePair(serverCert, serverKey);
-                var cacert = File.ReadAllText("ssl/ca.crt");
+                //var serverCert = File.ReadAllText("ssl/server.crt");
+                //var serverKey = File.ReadAllText("ssl/server.key");
+                //var keypair = new KeyCertificatePair(serverCert, serverKey);
+                //var cacert = File.ReadAllText("ssl/ca.crt");
 
-                var credentials = new SslServerCredentials(new List<KeyCertificatePair>() { keypair }, cacert, true);
+                //var credentials = new SslServerCredentials(new List<KeyCertificatePair>() { keypair }, cacert, true);
+
+                // the reflection service will be aware of "Greeter" and "ServerReflection" services.
+                var reflectionServiceImpl = new ReflectionServiceImpl(GreetingService.Descriptor, ServerReflection.Descriptor);
 
                 server = new Server()
                 {
-                    Services = { 
-                        GreetingService.BindService( new GreetingServiceImpl() ),
-                        SqrtService.BindService( new SqrtServiceImpl() ),
-                        CalculatorService.BindService( new CalculatorServiceImpl() ),
-                        GreetdeadlineService.BindService( new GreetdeadlineServiceImpl() )                    
+                    Services = {
+                        //GreetingService.BindService( new GreetingServiceImpl() ),
+                        //SqrtService.BindService( new SqrtServiceImpl() ),
+                        //CalculatorService.BindService( new CalculatorServiceImpl() ),
+                        //GreetdeadlineService.BindService( new GreetdeadlineServiceImpl() )
+                        GreetingService.BindService(new GreetingServiceImpl()),
+                        ServerReflection.BindService(reflectionServiceImpl)
                     },
                     Ports = { 
-                        new ServerPort("localhost", Port, credentials) 
+                        new ServerPort("localhost", Port, ServerCredentials.Insecure) //credentials) 
                     },
                     //Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
                 };
