49c8f85613e837fb85cef6c022df316debd72eab
Deyanira Gutiérrez Arteaga <deyaniragutierrezarteaga@gmail.com>
2022-01-18 09:59:17 -0600

    rpc "ListBlog" server stream method added and implemented in the server and client side

--- client/Program.cs
+++ client/Program.cs
@@ -19,14 +19,14 @@ namespace client
 
             var client = new BlogService.BlogServiceClient(channel);
 
+            //var newBlog = CreateBlog(client);
+
             //CreateBlog(client);
-            var newBlog = CreateBlog(client);
-
             //ReadBlog(client);
-
             //UpdateBlog(client, newBlog);
+            //DeleteBlog(client, newBlog);
 
-            DeleteBlog(client, newBlog);
+            await ListBlog(client);
 
             channel.ShutdownAsync().Wait();
             Console.ReadKey();
@@ -96,6 +96,16 @@ namespace client
             catch (RpcException e)
             {
                 Console.WriteLine(e.Status.Detail);
+            }
+        }
+
+        private static async Task ListBlog(BlogService.BlogServiceClient client)
+        {
+            var response = client.ListBlog(new ListBlogRequest() { });
+
+            while (await response.ResponseStream.MoveNext())
+            {
+                Console.WriteLine(response.ResponseStream.Current.Blog.ToString());
             }
         }
     }
